f393dc416c9c515675cb239f7e04d20c5a7f2387
N1covega <pavlo@mindnow.io>
2022-06-02 15:51:47 +0300

    added logs

--- ios/Classes/SwiftPdfMergePlugin.swift
+++ ios/Classes/SwiftPdfMergePlugin.swift
@@ -13,9 +13,11 @@ public class SwiftPdfMergePlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+       print("INVOKE")
      if call.method == "PdfMerger" {
+       print("INDSIDE")
        if let args = call.arguments as? Dictionary<String, Any>{
-        
+         print("INDSIDE2")
         DispatchQueue.global().async {
             let singlePDFFromMultiplePDF =  SwiftPdfMergePlugin.mergeMultiplePDF(args : args)
             DispatchQueue.main.sync {
@@ -93,7 +95,7 @@ public class SwiftPdfMergePlugin: NSObject, FlutterPlugin {
   class func mergeMultiplePDF(args: Dictionary<String, Any>) -> String? {
 
   do{
-
+            print("INDSIDE3")
             if let paths = args["paths"] as? [String], let outputDirPath = args["outputDirPath"] as? String {
 
                  guard UIGraphicsBeginPDFContextToFile(outputDirPath, CGRect.zero, nil) else {
